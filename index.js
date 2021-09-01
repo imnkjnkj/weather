94b71629bee5e39cc11042fa18b5d45b957d2d4f
imnkjnkj <65354236+imnkjnkj@users.noreply.github.com>
2021-09-01 15:10:58 +0700

    3rd

--- index.js
+++ index.js
@@ -97,3 +97,24 @@ const callApi = () => {
         });
 };
 callApi();
+
+
+var slideIndex = 1;
+// Next/previous controls
+function plusDivs(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+};
